18a0400a11b463cac61a8428a06c70c4cabe56cc
Defxult <52070034+Defxult@users.noreply.github.com>
2021-06-19 10:54:29 -0400

    Update version. New package description

--- reactionmenu/__init__.py
+++ reactionmenu/__init__.py
@@ -1,16 +1,18 @@
 """
-ReactionMenu
-~~~~~~~~~~~~
+reactionmenu • discord.py pagination
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-A package to create a discord.py reaction menu (paginator).
+A package to create a discord.py paginator. Supports pagination with Discords Buttons feature and reactions.
 
 :copyright: (c) 2021 Defxult#8269
 :license: MIT
 
 """
 
-from .buttons import Button, ButtonType
+from .buttons import Button, ButtonType, ComponentsButton
+from .buttons_menu import ButtonsMenu
 from .core import ReactionMenu
 from .text import TextMenu
-__version__ = '1.0.9'
+
+__version__ = '1.1.0'
 __source__ = 'https://github.com/Defxult/reactionmenu'
