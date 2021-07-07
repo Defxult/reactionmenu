750c88753473226dbfeff43467cb7ec8bfa9c03d
Defxult <52070034+Defxult@users.noreply.github.com>
2021-07-06 20:35:05 -0400

    Update version to `v2.0.2`

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ details = {
 setup(
     author='Defxult#8269',
     name='reactionmenu',
-    version='2.0.1', 
+    version='2.0.2', 
     description='A package to create a discord.py paginator. Supports pagination with Discords Buttons feature and reactions.',
     url='https://github.com/Defxult/reactionmenu',
     project_urls=details,
