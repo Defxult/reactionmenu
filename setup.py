766f1793472f574d9bfc02578e5660ac5fecdb4d
Defxult <52070034+Defxult@users.noreply.github.com>
2021-08-13 17:21:16 -0400

    Remove 3rd party dependencies

--- setup.py
+++ setup.py
@@ -57,5 +57,5 @@ setup(
     license='MIT',
     keywords=tags,
     packages=find_packages(),
-    install_requires=['discord.py>=1.4.0', 'dislash.py>=1.1.0']
+    install_requires=['discord.py>=2.0.0']
 )
